85fb1d0e22ce06285223078ed8c9c07d0d15eddf
Marivaldo Sena <msena.ifsp@gmail.com>
2017-10-09 00:32:10 -0300

    Inclusão dos campos referenes às sequências no formulário da janela modal.

--- resources/views/layouts/partials/sequencia-detalhes.blade.php
+++ resources/views/layouts/partials/sequencia-detalhes.blade.php
@@ -24,11 +24,52 @@
                         </label>
                     </div>
 
-                    <!-- Quantidade -->
+                    <!-- Quantidade de Recursos -->
                     <div class="form-group">
                         <label for="quantidade_recurso" class="control-label">Quantidade de Rercurso</label>
                         <input type="number" id="quantidade_recurso" name="quantidade_recurso[]"
                         class="form-control">
+                    </div>
+
+                    <!-- Data de Disponbilização do Recurso -->
+                    <div class="form-group">
+                        <label for="data_inicio_disp_recurso" class="control-label">
+                            Data de Disponibilização do Recurso
+                        </label>
+                        <input type="date" id="data_inicio_disp_recurso" name="data_inicio_disp_recurso"
+                            class="form-control">
+                    </div>
+
+                    <!-- Tempo Alocado -->
+                    <div class="form-group">
+                        <label for="tempo_alocado" class="control-label">
+                            Tempo Alocado:
+                        </label>
+                        <input type="time" id="tempo_alocado" name="tempo_alocado" class="form-control">
+                    </div>
+
+                    <!-- Início Otimista -->
+                    <div class="form-group">
+                        <label for="inicio_otimista" class="control-label">Início Otimista:</label>
+                        <input type="date" id="inicio_otimista" name="inicio_otimista" class="form-control">
+                    </div>
+
+                    <!-- Início Pessimista -->
+                    <div class="form-group">
+                        <label for="inicio_pessimista" class="control-label">Início Pessimista:</label>
+                        <input type="date" id="inicio_pessimista" name="inicio_pessimista" class="form-control">
+                    </div>
+
+                    <!-- Fim Otimista -->
+                    <div class="form-group">
+                        <label for="fim_otimista" class="control-label">Fim Otimista:</label>
+                        <input type="date" id="fim_otimista" name="fim_otimista" class="form-control">
+                    </div>
+
+                    <!-- Fim Pessimista -->
+                    <div class="form-group">
+                        <label for="fim_pessimista" class="control-label">Fim Pessimista:</label>
+                        <input type="date" id="fim_pessimista" name="fim_pessimista" class="form-control">
                     </div>
                 </form>
             </div>
